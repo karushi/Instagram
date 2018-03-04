0a11b8e414b0ca6abea38c2db44046d0bb184cfb
karushi <you@example.com>
2018-03-04 07:55:26 +0300

    create image views

--- insta/gram/views.py
+++ insta/gram/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
+def welcome(request):
+    image_pics = Image.objects.all()
+    return render(request,'index.html',{"image_pics":image_pics})
