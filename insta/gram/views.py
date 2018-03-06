beb247289e75a60c010391cab440afdd26758703
karushi <you@example.com>
2018-03-06 10:26:06 +0300

    create login method

--- insta/gram/views.py
+++ insta/gram/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from .models import Image
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404
+from .models import Image,Profile
 
 # Create your views here.
+ 
+@login_required(login_url='/accounts/login/')
 def welcome(request):
+    title = 'Instagram'
     image_pics = Image.objects.all()
-    return render(request,'index.html',{"image_pics":image_pics})
+    return render(request,'index.html',{"title":title,"image_pics":image_pics})
+
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    title = 'Insta'
+    profile = Profile.objects.all()
+    return render(request,'profile/profile.html',{"title":title, "profile":profile})  
+
+  
