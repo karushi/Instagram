a10c192443367467760f824c2bc14a58a0084f2a
karushi <you@example.com>
2018-03-06 10:24:01 +0300

    create tasts

--- insta/gram/tests.py
+++ insta/gram/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from .models import Profile, Image
 # Create your tests here.
+class ImageTestClass(TestCase):
+     
+    def setUp(self):
+         self.image=Image(image='image',image_name="landscape",image_caption="beautiful",likes="100",imgae=karush)
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_save(self):
+        self.imgae.save_imgaes()
+        imgae=imgae.objects.all()
+        self.assertTrue(len(imgaes)>0)  
+
+    def test_delete(self):
+        self.image.save_images()
+        images=Image.objects.all()
+        self.image.delete_images()
+        images=Image.objects.all()
+        self.assertTrue(len(images)==0)    
